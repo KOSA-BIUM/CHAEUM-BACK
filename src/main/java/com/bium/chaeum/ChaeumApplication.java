2ea1faa7c3f1ebda8ca0253e100cc4902fba9191
ncb6206 <ncb6206@gmail.com>
2025-10-10 13:47:26 +0900

    Init : 초기 환경 설정

---
+++ src/main/java/com/bium/chaeum/ChaeumApplication.java
@@ -0,0 +1,13 @@
+package com.bium.chaeum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChaeumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChaeumApplication.class, args);
+	}
+
+}
